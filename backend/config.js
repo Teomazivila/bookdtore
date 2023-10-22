962fb026ab0ebdbe08f80e6c45a73e7b5e82dcbc
teodoricom@hotmail.com <teodoricom@hotmail.com>
2023-10-22 23:00:37 +0100

    Enable heroku CI-CD

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://admin:zL3kcDlWpsbeLvL0@cluster0.xjrmo6y.mongodb.net/BookStore?retryWrites=true&w=majority";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
