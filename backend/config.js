7541b15950911323ef123ed5eedf9dc31234e997
teodoricom@hotmail.com <teodoricom@hotmail.com>
2023-10-22 23:56:31 +0100

    remove secrets

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  "mongodb+srv://admin:zL3kcDlWpsbegit0@cluster0.xjrmo6y.mongodb.net/BookStore?retryWrites=true&w=majority";
+  "mongodb+srv://admin:@cluster0.xjrmo6y.mongodb.net/BookStore?retryWrites=true&w=majority";
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
